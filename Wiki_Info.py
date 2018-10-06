f43520b3f8cface229cce5dbc2a0820d78e421d8
Anirudh Prakash <aprakash598@gmail.com>
2018-10-07 01:23:30 +0530

    Created a Basic UI

--- Wiki_Info.py
+++ Wiki_Info.py
@@ -1,15 +1,15 @@
 import requests
 import wikipedia
 from lxml import etree
-url = 'https://en.wikipedia.org/wiki/Massachusetts_Institute_of_Technology'
-req = requests.get(url)
-store = etree.fromstring(req.text)
-output_motto = store.xpath('//table[@class="infobox vcard"]/tbody/tr[th/text()="Motto"]/td/i')
-output_loc = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[15]/td/span[1]/a')
-output_pres = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[9]/td/a')
-output_chan = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[8]/td/a')
-print("Motto : "+output_motto[0].text)
-print("President : "+output_pres[0].text)
-print("Chancellor : "+output_chan[0].text)
-print("Location : "+output_loc[0].text)
+def scraper(name):
+    url = 'https://en.wikipedia.org/wiki/'+name
+    req = requests.get(url)
+    store = etree.fromstring(req.text)
+    output_motto = store.xpath('//table[@class="infobox vcard"]/tbody/tr[th/text()="Motto"]/td/i')
+    output_loc = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[15]/td/span[1]/a')
+    output_pres = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[9]/td/a')
+    output_chan = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[8]/td/a')
+    dict_items={'Motto':output_motto[0].text, 'President':output_pres[0].text, 'Chancellor':output_chan[0].text, 'Location':output_loc[0].text}
+
+    return (dict_items)
 
